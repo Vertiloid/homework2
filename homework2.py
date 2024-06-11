87e9dd44c93cc981f6184bb503860e15412a095c
Vertiloid <647214036@rambler.ru>
2024-06-11 19:43:27 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+task_time = 1.5 / 12
+print('Курс:', course_name + ',', 'всего задач:', completed_tasks, 'затрачено часов:', str(hours_spent) + ',', 'среднее время выполнения', task_time, 'часа.')
